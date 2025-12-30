1ecd112bfdaf9c6273495978c9dac35e72f5a3b6
FaithLink_portal <sukmanjung0501@gmail.com>
2025-12-30 16:32:40 +0000

    Reduce left padding to align grid left

--- src/index.tsx
+++ src/index.tsx
@@ -2697,7 +2697,7 @@ app.get('/game/simple/sudoku/play', (c) => {
             
             /* 컨텐츠 영역 */
             .modal-body {
-                padding: 15px;
+                padding: 10px 15px 15px 10px;
                 display: flex;
                 flex-direction: row;
                 gap: 15px;
